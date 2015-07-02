575a83e7e030c5e48b1bda2b5aab996b274cbf92
Cyrille Giquello <cyrille@giquello.fr>
2015-07-03 00:51:09 +0200

    typo

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -27,31 +27,32 @@ class ApiController extends Controller
      *
      * Order : priority, play_time, id
      *
-     * @param unknown $channelId
+     * @param unknown $channelId            
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function getMessagesSet($channelId)
     {
         $d = new \Carbon\Carbon();
-     
+        
         $q = Message::forChannel($channelId)->notDone()
             ->orderBy('priority', 'desc')
             ->orderBy('play_at_time', 'desc')
-            ->orderBy('id', 'asc')            
-            ->limit(2)
-            ;
+            ->orderBy('id', 'asc')
+            ->limit(2);
+        
+        // to force seconds to 00
+        $time_hms = str_pad($d->hour, 2, '0', STR_PAD_LEFT) . ':' . str_pad($d->minute, 2, '0', STR_PAD_LEFT) . ':00';
 
-        $time_hms = str_pad($d->hour, 2, '0', STR_PAD_LEFT).':'.str_pad($d->minute, 2, '0', STR_PAD_LEFT).':00';
-        error_log( 'play_at_time: '. $time_hms );
+        // error_log( 'play_at_time: '. $time_hms );
+        
         // and a nested query for 'play_at_time'
         $m = new Message();
         $q2 = $m->newQueryWithoutScopes()
-            //->where('play_at_time', '=', '12:00:00')
-            ->where('play_at_time', '=', $time_hms )
+            ->where('play_at_time', '=', $time_hms)
             ->orWhere('play_at_time', '=', null);
-        $q->addNestedWhereQuery( $q2->getQuery() );
+        $q->addNestedWhereQuery($q2->getQuery());
         
-        error_log($q->toSql());
+        // error_log($q->toSql());
         $messagesSet = $q->get();
         
         return response()->json($messagesSet);
